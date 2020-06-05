f4eca60d84d839ac6ca6256ff651951e946cb923
Chris Grounds <chrisbacon2009@hotmail.com>
2020-06-05 21:06:04 +0100

    Updated readme and refactored bash into functions

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ This is a demonstration of a secure online voting system, using public-key infra
 
 ## How it works
 
-Similar to the Estonian i-voting system, this system has two layers. The first layer takes the input vote and encrypts it with the central, root server public key. This provides confidentially, ensuring no unauthorised person can view what the vote is, so long as the root server private key is not compromised. The second layer then provides the means to authenticate the voter. This is achieved by taking the encrypted vote from the first layer, hashing it with SHA-256 and then signing it with the voter's private key, thereby creating a digital signature. Anyone can then verify that the vote is indeed from the voter by using the voter's public key to decrypt the signature, thereby unwrapping the encryption and revealing the SHA-256 hash of the encrypted vote. This hash can then be comapred with the provided encrypted vote after it has been hashed with SHA-256.
+Similar to the Estonian i-voting system, this system has two layers. 
+
+The first layer takes the input vote and encrypts it with the central, root server public key. This provides confidentially, ensuring no unauthorised person can view what the vote is, so long as the root server private key is not compromised. 
+
+The second layer then provides the means to authenticate the voter. This is achieved by taking the encrypted vote from the first layer, hashing it with SHA-256 and then signing (encrypting) it with the voter's private key, thereby creating a digital signature. Anyone can then verify that the vote is indeed from the voter by using the voter's public key to decrypt the signature, thereby unwrapping the encryption and revealing the SHA-256 hash of the encrypted vote. This hash can then be comapred to the provided encrypted vote after it has also been hashed with SHA-256.
+
+All in all, this means the voter sends their encrypted vote along with their signature. The root server verifies the signature and then decrypts the vote, adding it to the voting tally.
--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ HASHED_ENCRYPTED_MSG=$NAME/hashed-msg.enc
 ROOT_PRIVATE_KEY=root-server/root-key.pem
 ROOT_PUBLIC_KEY=root-server/root-key.pub.pem
 
-PRIVATE_KEY=$NAME/key.pem
-PUBLIC_KEY=$NAME/key.pub.pem
-SIGNATURE=$NAME/signature.sign
+VOTER_PRIVATE_KEY=$NAME/key.pem
+VOTER_PUBLIC_KEY=$NAME/key.pub.pem
+VOTER_SIGNATURE=$NAME/signature.sign
 
 rm -rf root-server
 rm -rf $NAME
@@ -16,30 +16,47 @@ rm -rf $NAME
 mkdir root-server
 mkdir $NAME
 
-#### Key creation ####
-# Generate RSA private key for root server
-openssl genrsa -out $ROOT_PRIVATE_KEY 4096 >& /dev/null
-# Generate public key from private key for root server
-openssl rsa -in $ROOT_PRIVATE_KEY -pubout >& $ROOT_PUBLIC_KEY
+function generateRSAPrivateKey {
+    local outFile=$1
 
-# Generate RSA private key for voter
-openssl genrsa -out $PRIVATE_KEY 4096 >& /dev/null
-# Generate public key for voter from voter's private key
-openssl rsa -in $PRIVATE_KEY -pubout >& $PUBLIC_KEY
+    openssl genrsa -out $outFile 4096 >& /dev/null
+}
+
+function extractPublicKeyFromPrivateKey {
+    local inPrivateKey=$1
+    local outFile=$2
+
+    openssl rsa -in $inPrivateKey -pubout >& $outFile
+}
+
+function encryptWithRootPrivateKey {
+    echo $MSG | openssl enc -aes-256-cbc -pass file:$ROOT_PRIVATE_KEY -out $ENCRYPTED_MSG
+}
+
+function createHash {
+    cat $ENCRYPTED_MSG | shasum -a 256 > $HASHED_ENCRYPTED_MSG
+}
+
+function createSignature {
+    openssl dgst -sha256 -sign $VOTER_PRIVATE_KEY $HASHED_ENCRYPTED_MSG > $VOTER_SIGNATURE
+}
+
+function verifySignature {
+    openssl dgst -sha256 -verify $VOTER_PUBLIC_KEY -signature $VOTER_SIGNATURE $HASHED_ENCRYPTED_MSG
+}
+
+#### Key creation ####
+generateRSAPrivateKey $ROOT_PRIVATE_KEY
+extractPublicKeyFromPrivateKey $ROOT_PRIVATE_KEY $ROOT_PUBLIC_KEY
+
+generateRSAPrivateKey $VOTER_PRIVATE_KEY
+extractPublicKeyFromPrivateKey $VOTER_PRIVATE_KEY $VOTER_PUBLIC_KEY
 
 #### Encyrpting ####
-# Use the root public key to encrypt the message
-echo $MSG | openssl enc -aes-256-cbc -pass file:$ROOT_PRIVATE_KEY -out $ENCRYPTED_MSG
+encryptWithRootPrivateKey 
 
 #### Signing ####
-# Use SHA-256 to hash STDIN and write to file
-cat $ENCRYPTED_MSG | shasum -a 256 > $HASHED_ENCRYPTED_MSG
-# Encrypt the hashed_encrypted_msg with the voter's private key to create a digital signature
-openssl dgst -sha256 -sign $PRIVATE_KEY $HASHED_ENCRYPTED_MSG > $SIGNATURE
+createHash
+createSignature
 
-# We can now verify that the message comes from the source
-# They send the message with the signature, and we can verify the signature, thereby verifying they created it (by using their public key to decrypt it and comparing it to the message hash)
-# The purpose of the signature is to *authenticate*
-# Verify signature by decrypting the signature with voter's public key and comparing the hash with the encrypted_hashed_message
-openssl dgst -sha256 -verify $PUBLIC_KEY -signature $SIGNATURE $HASHED_ENCRYPTED_MSG
-
+verifySignature
